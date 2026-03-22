43e36d0fb5da703d85549b9cdfae649402cdcce8
stephen <lee.stephen@courrier.uqam.ca>
2026-03-22 11:45:08 -0400

    adding main

---
+++ backend/screeningApplication/src/main/java/com/crypto/screener/screeningApplication/screeningApplication.java
@@ -0,0 +1,11 @@
+package com.crypto.screener.screeningApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class screeningApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(screeningApplication.class, args);
+    }
+}
